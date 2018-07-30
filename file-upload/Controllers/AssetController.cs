53aeccbf62bf773c0f437caaf647cbbcc085a4cf
Martin Brandl <mjibrandl@gmail.com>
2018-07-30 18:22:33 +0200

    Implemented file upload feature within the API

---
+++ file-upload/Controllers/AssetController.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace file_upload.Controllers
+{
+    [Route("api/[controller]")]
+    public class AssetController : Controller
+    {
+        [HttpPost]
+        public IActionResult UploadAsset([FromForm]IFormFile asset)
+        {
+            return Ok();
+        }
+
+
+
+    }
+}
